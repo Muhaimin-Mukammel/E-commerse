69d25d46bb862d98eaf1531cc248ee0225ddb305
Muhaimin Mukammel <muhaiminmukammel818@gmail.com>
2026-09-07 21:02:20 +0600

    configuring multiple database in progress
    just do the sql and application.properties part, only need to add the datasource and entitymanagerfactory

--- infrastructure/postgres/db.sql
+++ infrastructure/postgres/db.sql
@@ -1,9 +1,17 @@
 CREATE DATABASE user_mode_db;
-
 CREATE DATABASE merchant_mode_db;
-
 CREATE DATABASE admin_mode_db;
-
 CREATE DATABASE shared_db;
-
 CREATE DATABASE keycloak_db;
+
+CREATE USER userlocal WITH PASSWORD "userlocal1234";
+CREATE USER merchantlocal WITH PASSWORD "merchantlocal1234";
+CREATE USER adminlocal WITH PASSWORD "adminlocal1234";
+CREATE USER shared WITH PASSWORD "shared1234";
+CREATE USER keycloak WITH PASSWORD "keycloak1234";
+
+GRANT ALL PRIVILEGES ON DATABASE user_mode_db TO userlocal;
+GRANT ALL PRIVILEGES ON DATABASE merchant_mode_db TO merchantlocal;
+GRANT ALL PRIVILEGES ON DATABASE admin_mode_db TO adminlocal;
+GRANT ALL PRIVILEGES ON DATABASE shared_db TO shared;
+GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO keycloak;
