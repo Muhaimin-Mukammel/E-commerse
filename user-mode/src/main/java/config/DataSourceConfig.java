b4920c4888869afa70a4ec12bf1e8e575e069194
Muhaimin Mukammel <muhaiminmukammel818@gmail.com>
2026-09-07 20:41:59 +0600

    starting toe configure the databases

---
+++ user-mode/src/main/java/config/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "localDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.local")
+    public DataSource localDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "sharedDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.shared")
+    public DataSource sharedDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+
+}
