c2f1de5e5d3b3fb02e75a8f01c60c7ecbb9d492b
Muhaimin Mukammel <muhaiminmukammel818@gmail.com>
2026-09-21 03:49:10 +0600

    debbuged a lot of things and finally the user mode is started
    currently testing

--- user-mode/src/main/java/com/ecomerse/usermode/UserModeApplication.java
+++ user-mode/src/main/java/com/ecomerse/usermode/UserModeApplication.java
@@ -1,7 +1,9 @@
 package com.ecomerse.usermode;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class UserModeApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserModeApplication.class, args);
